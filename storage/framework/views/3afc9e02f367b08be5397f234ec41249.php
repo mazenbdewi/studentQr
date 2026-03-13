4c016269fb35f8f711a67265dcbf9f4daecb8117
Clauda <klodaalrakkad@gmail.com>
2026-03-13 13:47:48 +0300

    add /storage/framework/views/* to gitignore

--- storage/framework/views/3afc9e02f367b08be5397f234ec41249.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['fi-dropdown-list'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /home/mazen/Documents/programes/StudentAttendenceSystem/resources/views/vendor/filament/components/dropdown/list/index.blade.php ENDPATH**/ ?>
