b2238d5bf2014b61015ee0164c5858230cfa8503
Mazen Bdewi <mazen.bdewi@gmail.com>
2026-07-27 12:27:20 +0300

    stable 1

--- resources/views/filament/pages/academic-term-management.blade.php
+++ resources/views/filament/pages/academic-term-management.blade.php
@@ -1,7 +1,5 @@
 <x-filament-panels::page>
-    <div class="p-4 border rounded-lg border-warning-300 bg-warning-50 text-warning-900 dark:bg-warning-950 dark:text-warning-100">
-        يحدد هذا الجدول الفصل الدراسي الحالي من خلال إعداد النظام الوحيد. لن يتم حذف بيانات الفصول السابقة عند تغيير الفصل الحالي.
-    </div>
+ 
 
     {{ $this->table }}
 </x-filament-panels::page>
